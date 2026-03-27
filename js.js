78df79281830e3e360b916a855563c9b3d57e113
bhanu917 <bhanuprakashu1@gmail.com>
2026-03-27 20:33:08 +0530

    first commit

---
+++ js.js
@@ -0,0 +1,73 @@
+
+
+fetch("https://fakestoreapi.com/products")
+    .then(res => res.json())
+    .then(data => {
+        localStorage.setItem("allproducts", JSON.stringify(data));
+    });
+
+let allp = document.getElementById("allp");
+let mensp = document.getElementById("mensp");
+let womensp = document.getElementById("womensp");
+let jewerlyp = document.getElementById("jewerlyp");
+let electronicp = document.getElementById("electronicp");
+
+allp.addEventListener("click", () => showdata());
+mensp.addEventListener("click", () => showdata("men's clothing"));
+womensp.addEventListener("click", () => showdata("women's clothing"));
+jewerlyp.addEventListener("click", () => showdata("jewelery"));
+electronicp.addEventListener("click", () => showdata("electronics"));
+
+
+
+
+
+
+
+
+let prodcontainer = document.getElementById("prod-container");
+function showdata(category) {
+    let allproducts = JSON.parse(localStorage.getItem("allproducts"));
+    console.log(allproducts);
+    if (category) {
+        allproducts = allproducts.filter(p => p.category === category);
+    }
+    prodcontainer.innerHTML = "";
+
+    allproducts.forEach(product => {
+        let cards = document.createElement("div");
+        cards.className = "prod-card";
+        cards.innerHTML = `
+        <div class="imgid">
+            <img src="${product.image}" width="250" height="300">
+        </div>
+        <div class="p-content">
+            <p style="margin-top:20px;font-size:20px"><b>${product.title.substring(0, 25)}...</b></p>
+            <p>${product.description.substring(0, 80)}...</p>
+            <hr style="width:80%;margin-left:9%">
+            <p><b>${product.price}$</b></p>
+            <hr style="width:80%;margin-left:9%">
+            <p class="card-butts">
+                <button>details</button>
+                <button class="cart-butt">add to cart</button>
+            </p>
+        </div>
+    `;
+
+        prodcontainer.append(cards);
+
+        // select the button inside this card
+        let cartbutt = cards.querySelector(".cart-butt");
+        cartbutt.addEventListener("click", () => {
+            let cart = JSON.parse(localStorage.getItem("cart")) || [];
+            cart.push(product.id);
+            localStorage.setItem("cart", JSON.stringify(cart));
+            console.log("Cart updated:", cart);
+        });
+    });
+
+}
+
+
+
+showdata();
